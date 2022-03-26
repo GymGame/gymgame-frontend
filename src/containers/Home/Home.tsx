4c2f962484b5248d7aa8966f8fb29b83a62a8ec5
Zhenghao Jia <cherie-xia@hotmail.com>
2022-03-26 23:26:08 +1100

    refactor routing

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import PlaySection from './components/PlaySection';
-import Screen from '../../components/Screen';
 import MeetTheGymJunkieSection from './components/MeetTheGymJunkieSection';
 import HowDoIGetBigSection from './components/HowDoIGetBigSection';
+
 const Home = () => {
   return (
-    <Screen>
+    <>
       <PlaySection />
       <MeetTheGymJunkieSection />
       <HowDoIGetBigSection />
-    </Screen>
+    </>
   );
 };
 
